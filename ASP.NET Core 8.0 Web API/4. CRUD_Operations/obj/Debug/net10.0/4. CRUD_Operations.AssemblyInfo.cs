fa77f4875632641ac1b216dd1c7b14692e2c294d
AnishaKumari82 <anishabrnwl@gmail.com>
2026-07-18 01:08:52 +0530

    Added React Lab 1 - My First React Application

--- ASP.NET Core 8.0 Web API/4. CRUD_Operations/obj/Debug/net10.0/4. CRUD_Operations.AssemblyInfo.cs
+++ ASP.NET Core 8.0 Web API/4. CRUD_Operations/obj/Debug/net10.0/4. CRUD_Operations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4. CRUD_Operations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ba82041a842fee0b7ab3d0b9f3a16b35190949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7829ca02a6d60e9b9b337fd19fced4ac919fe2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("4. CRUD_Operations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4. CRUD_Operations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Entity-Framework-Core-8/Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations/obj/Debug/net10.0/Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations.AssemblyInfo.cs
+++ Entity-Framework-Core-8/Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations/obj/Debug/net10.0/Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ba82041a842fee0b7ab3d0b9f3a16b35190949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7829ca02a6d60e9b9b337fd19fced4ac919fe2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab-3-Using-EF-Core-CLI-to-Create-and-Apply-Migrations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
